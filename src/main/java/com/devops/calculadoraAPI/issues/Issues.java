ee0d0560bd683d921867c384b53cbf7d25ee503d
HelisB <70779171+HelisB@users.noreply.github.com>
2020-10-09 18:39:38 -0500

    Update Issues.java
    corregi  codigo

--- src/main/java/com/devops/calculadoraAPI/issues/Issues.java
+++ src/main/java/com/devops/calculadoraAPI/issues/Issues.java
@@ -1,4 +1,138 @@
-eEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+package com.devops.calculadoraAPI.issues;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.regex.Pattern;
+
+import javax.naming.Context;
+import javax.naming.directory.DirContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+public class Issues extends HttpServlet {
+
+	@RequestMapping
+	private String greet(String greetee) {
+		return null;
+	}
+
+	
+	void LdapERROR() {
+		// Set up the environment for creating the initial context
+		Hashtable<String, Object> env = new Hashtable<String, Object>();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		env.put(Context.PROVIDER_URL, "ldap://localhost:389/o=JNDITutorial");
+
+		// Use anonymous authentication
+		env.put(Context.SECURITY_AUTHENTICATION, "none"); // Noncompliant
+
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String referer = request.getHeader("referer"); // Noncompliant
+		if (isTrustedReferer(referer)) {
+			// ..
+		}
+		// ...
+	}
+
+	private boolean isTrustedReferer(String referer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	 String regex; // a regular expression
+	  String input; // a user input
+
+	  void foo(CharSequence htmlString) {
+	    input.matches(regex);  // Questionable
+	    Pattern.compile(regex);  // Questionable
+	    Pattern.compile(regex, Pattern.CASE_INSENSITIVE);  // Questionable
+
+	    String replacement = "test";
+	    input.replaceAll(regex, replacement);  // Questionable
+	    input.replaceFirst(regex, replacement);  // Questionable
+
+	    if (!Pattern.matches(".*<script>.*", htmlString)) { // Questionable, even if the pattern is hard-coded
+	    }
+	  }
+	  
+	  
+	  @RequestMapping
+		private String greet1(String greetee) {
+		  int h =0;
+			 if(h!=0) {}
+			return null;
+		}
+
+		
+		void LdapERROR1() {
+			// Set up the environment for creating the initial context
+			Hashtable<String, Object> env = new Hashtable<String, Object>();
+			env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+			env.put(Context.PROVIDER_URL, "ldap://localhost:389/o=JNDITutorial");
+
+			// Use anonymous authentication
+			env.put(Context.SECURITY_AUTHENTICATION, "none"); // Noncompliant
+
+	
+		}
+		
+		protected void doPost1(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			String referer = request.getHeader("referer"); // Noncompliant
+			if (isTrustedReferer(referer)) {
+				// ..
+			}
+			// ...
+		}
+
+		private boolean isTrustedReferer1(String referer) {
+			// TODO Auto-generated method stub
+			int h =0;
+			 if(h!=0) {}
+			return false;
+		}
+		
+		
+		 String regex1; // a regular expression
+		  String input1; // a user input
+
+		  void foo1(CharSequence htmlString) {
+		    input1.matches(regex1);  // Questionable
+		    Pattern.compile(regex1);  // Questionable
+		    Pattern.compile(regex1, Pattern.CASE_INSENSITIVE);  // Questionable
+
+		    String replacement = "test";
+		    input1.replaceAll(regex1, replacement);  // Questionable
+		    input1.replaceFirst(regex1, replacement);  // Questionable
+
+		    if (!Pattern.matches(".*<script>.*", htmlString)) { // Questionable, even if the pattern is hard-coded
+		    }
+		  }
+		  
+		  
+		  @RequestMapping
+			private String greet2(String greetee) {
+			  String firstName = "h"; // String overrides equals
+			  String lastName = "g";
+
+			  if (firstName == lastName) { return "h"; }
+			  return null;
+			}
+
+			
+			void LdapERROR2() {
+				// Set up the environment for creating the initial context
+				Hashtable<String, Object> env = new Hashtable<String, Object>();
+				env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
 				env.put(Context.PROVIDER_URL, "ldap://localhost:389/o=JNDITutorial");
 
 				// Use anonymous authentication
